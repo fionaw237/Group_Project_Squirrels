2a9ff5d7a61adc6aefa61b2b4ad17db49b61e071
fddata <fraserjdouglas@gmail.com>
2018-09-08 20:54:05 +0100

    adds basic map view to home screen - no functionality yet

--- client/src/app.js
+++ client/src/app.js
@@ -1,13 +1,20 @@
 const Sightings = require('./models/sightings.js');
 const SightingFormView = require('./views/sighting_form_view.js');
+const MapView = require('./views/map_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('#new-sighting-form');
-  const sightingFormView = new SightingFormView(form);
-  sightingFormView.setUpEventListeners();
+  // const form = document.querySelector('#new-sighting-form');
+  // const sightingFormView = new SightingFormView(form);
+  // sightingFormView.setUpEventListeners();
 
 
   const sightingsData = new Sightings();
   sightingsData.setUpEventListeners();
+
+
+
+  const mainMap = document.querySelector('#map-container');
+  const mapView = new MapView(mainMap);
+  mapView.bindEvents();
 
 });
--- client/src/views/map_view.js
+++ client/src/views/map_view.js
@@ -2,4 +2,20 @@ const PubSub = require('../helpers/pub_sub.js');
 
 const MapView = function(container){
   this.container = container;
-}
+  this.coords = [55.0, -3.5]; //  centres default view to UK
+  this.map = L.map(this.container);
+  this.osmLayer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+  this.map.setView(this.coords, 5).addLayer(this.osmLayer); // note '5' is zoom level
+  this.mapMarkers = [];
+};
+
+
+
+MapView.prototype.bindEvents = function () {
+
+  PubSub.subscribe('Sightings:all-map-data-loaded', (event) => {
+    console.log(event.detail);
+  })
+};
+
+module.exports = MapView;
