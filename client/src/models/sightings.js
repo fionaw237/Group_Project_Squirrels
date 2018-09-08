98c2e13df2dd64b4ffdcffba11353cac04fb7fa6
Fiona Wilson <fionaw237@gmail.com>
2018-09-08 10:29:04 +0100

    Uncomments add new sighting functions

--- client/src/models/sightings.js
+++ client/src/models/sightings.js
@@ -10,19 +10,19 @@ Sightings.prototype.setUpEventListeners = function(){
   PubSub.subscribe('SightingFormView:sighting-submitted', (event) => {
     const newSighting = event.detail;
     console.log(newSighting);
-    // this.add(newSighting);
+    this.add(newSighting);
   });
 };
 
 
-// this.prototype.add = function(item){
-//   this.request
-//   .post(item)
-//   .then((sightings) => {
-//     this.items = sightings;
-//     PubSub.publish('Sightings:all-map-data-loaded', this.items);
-//   })
-//   .catch((err) => console.error(err));
-// }
+this.prototype.add = function(item){
+  this.request
+  .post(item)
+  .then((sightings) => {
+    this.items = sightings;
+    PubSub.publish('Sightings:all-map-data-loaded', this.items);
+  })
+  .catch((err) => console.error(err));
+}
 
 module.exports = Sightings;
