bc0d1488662a7ee6f31da7cba9a2ede64d259457
fddata <fraserjdouglas@gmail.com>
2018-09-10 12:16:40 +0100

    adds marker on form_map_view on click

--- client/src/views/form_map_view.js
+++ client/src/views/form_map_view.js
@@ -7,28 +7,28 @@ const FormMapView = function(container){
   this.map = L.map(this.container, {preferCanvas: true});
   this.osmLayer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
   this.map.setView(this.coords, 5).addLayer(this.osmLayer); // note '5' is zoom level
-  this.mapMarkers = [];
   this.coords = [];
 
-  // this.map.on('click',
-  //         function(e){
-  //           var coord = e.latlng.toString().split(',');
-  //           var lat = coord[0].split('(');
-  //           var lng = coord[1].split(')');
-  //           this.coords = [lat[1], lng[0].replace(/\s+/, "")]; //strips whitespace
-  //           });
 }
 
 FormMapView.prototype.bindEvents = function () {
 
+  var markerGroup = L.featureGroup();
+
   this.map.on('click',
           function(e){
-            var coord = e.latlng.toString().split(',');
-            var lat = coord[0].split('(');
-            var lng = coord[1].split(')');
-            this.coords = [lat[1], lng[0].replace(/\s+/, "")]; //strips whitespace
 
-  PubSub.publish('FormMapView:coords-ready', this.coords);
+            markerGroup.clearLayers();
+            const coord = e.latlng.toString().split(',');
+            const lat = coord[0].split('(');
+            const lng = coord[1].split(')');
+            this.coords = [lat[1], lng[0].replace(/\s+/, "")]; //strips whitespace
+            const marker = L.marker(this.coords);
+            marker.addTo(markerGroup)
+            markerGroup.addTo(this)
+
+
+            PubSub.publish('FormMapView:coords-ready', this.coords);
   });
 };
 
