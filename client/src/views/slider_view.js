2aa2f880cff3729042cbe3c0a045468f1769c85b
Fiona Wilson <fionaw237@gmail.com>
2018-09-11 13:16:44 +0100

    Makes minor changes

--- client/src/views/slider_view.js
+++ client/src/views/slider_view.js
@@ -14,7 +14,6 @@ SliderView.prototype.bindEvents = function(){
 
     this.element.addEventListener('change', (event) => {
       const selectedYear = event.target.value;
-      console.log(selectedYear);
       PubSub.publish('SliderView:selected-year-ready', selectedYear);
     })
 
